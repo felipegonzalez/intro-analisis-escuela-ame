7487587e99dfa626f2eeab3fe4e329c4ecb6574f
Felipe Gonzalez <felipexgonzalez@gmail.com>
2019-09-05 00:35:22 -0500

    Reordenar y mejorar gráficas

--- R/funciones_auxiliares.R
+++ R/funciones_auxiliares.R
@@ -15,3 +15,22 @@ cuantil <- function(x, probs = c(0,0.25, 0.5, 0.75,1), ...){
     cuantil_nom <- probs
     tibble(cuantil = cuantil_nom, valor = valores) 
 }
+
+grafica_cuantiles <- function(datos, grupo, valor){
+    if(!(".sample" %in% names(datos))){
+        datos$.sample <- 1
+    }
+    
+    cuantiles_tbl <- datos %>% group_by({{ grupo }}, .sample) %>% 
+        summarise(
+            num = n(),
+            cuantiles = list(cuantil({{ valor }}, c(0.1, 0.25, 0.5, 0.75, 0.9)))) %>% 
+        unnest 
+    
+    grafica <- ggplot(cuantiles_tbl  %>% spread(cuantil, valor), 
+        aes(x = {{ grupo }}, y = `0.5`)) +
+        geom_linerange(aes(ymin= `0.1`, ymax = `0.9`), colour = "gray40") +
+        geom_linerange(aes(ymin= `0.25`, ymax = `0.75`), size = 2, colour = "gray") 
+        geom_point(colour = "salmon", size = 2) 
+    grafica
+}
