d0b6a839b6cbf39574d503bed5a955a1b4f3c470
Felipe Gonzalez <felipexgonzalez@gmail.com>
2019-09-22 22:52:41 -0500

    Agregar lista de paquetes

---
+++ R/paquetes.R
@@ -0,0 +1,2 @@
+paquetes <- c("tidyverse", "ggrepel", "knitr", "bookdown", "reshape2", "kableExtra", "ggthemes", "nullabor", "glmnet", "lubridate", "reshape2", "rfinterval", "ranger", "gridExtra", "caret")
+install.packages(paquetes)
